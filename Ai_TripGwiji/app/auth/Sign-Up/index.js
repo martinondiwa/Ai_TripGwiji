be52ff3859571fc337f0851daeade9ffa649cc3d
martinondiwa <surekondiwa@gmail.com>
2024-09-24 15:09:35 +0300

    sdfg

--- Ai_TripGwiji/app/auth/Sign-Up/index.js
+++ Ai_TripGwiji/app/auth/Sign-Up/index.js
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { useNavigation } from 'expo-router';
 export default function SignUp() {
-  const navigation=UserActivation();
+  const navigation=useNavigation();
   return (
     <View>
       <Text>SignUp</Text>
