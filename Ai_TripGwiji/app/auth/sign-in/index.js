043f7f8383cc4c2e759e1c1c3ba8eb278e019585
martinondiwa <surekondiwa@gmail.com>
2024-09-24 14:34:00 +0300

    asfgh

--- Ai_TripGwiji/app/auth/sign-in/index.js
+++ Ai_TripGwiji/app/auth/sign-in/index.js
@@ -1,64 +1,80 @@
-import { View, Text, TextInput } from 'react-native'
-import React, { useEffect } from 'react'
-import {useNavigation} from 'expo-router'
-import {Colors} from './../../../constants/Colors'
-import { StyleSheet } from 'react-native'
+import { View, Text, TextInput } from "react-native";
+import React, { useEffect } from "react";
+import { useNavigation } from "expo-router";
+import { Colors } from "./../../../constants/Colors";
+import { StyleSheet } from "react-native";
 export default function SignIn() {
-  const navigation=useNavigation();
-   useEffect(()=>{
+  const navigation = useNavigation();
+  useEffect(() => {
     navigation.setOptions({
-      headershown:false
-    })
-   },[])
+      headershown: false,
+    });
+  }, []);
   return (
     <View
-     style={{
-      padding:25,
-      marginTop:80,
-      backgroundColor:Colors.WHITE,
-      height:'100%'
-     }}
+      style={{
+        padding: 25,
+        marginTop: 80,
+        backgroundColor: Colors.WHITE,
+        height: "100%",
+      }}
     >
       <Text
-      style={{
-        fontFamily:'outfit-bold',
-        fontSize:30,
-      }} 
-      >Let's Sign You in</Text>
+        style={{
+          fontFamily: "outfit-bold",
+          fontSize: 30,
+        }}
+      >
+        Let's Sign You in
+      </Text>
       <Text
-      style={{
-        fontFamily:'outfit',
-        fontSize:30,
-        Color:Colors.GRAY,
-        marginTop:20
-      }} 
-      >Welcome Back</Text>
+        style={{
+          fontFamily: "outfit",
+          fontSize: 30,
+          Color: Colors.GRAY,
+          marginTop: 20,
+        }}
+      >
+        Welcome Back
+      </Text>
       <Text
-      style={{
-        fontFamily:'outfit',
-        fontSize:30,
-        Color:Colors.GRAY,
-        marginTop:10
-      }} 
-      >You have been missed</Text>
-      <View style={{
-        marginTop:50
-      }}>
-       <Text style={{fontFamily:'outfit'}}>Email</Text>
-       <TextInput
-       style={styles.input}
-        placeholder='Enter Email'></TextInput>
+        style={{
+          fontFamily: "outfit",
+          fontSize: 30,
+          Color: Colors.GRAY,
+          marginTop: 10,
+        }}
+      >
+        You have been missed
+      </Text>
+      
+      <View
+        style={{
+          marginTop: 50,
+        }}
+      >
+        <Text style={{ fontFamily: "outfit" }}>Email</Text>
+        <TextInput style={styles.input} placeholder="Enter Email"></TextInput>
+      </View>
+
+      <View
+        style={{
+          marginTop: 50,
+        }}
+      >
+        <Text style={{ fontFamily: "outfit" }}>Password</Text>
+        <TextInput style={styles.input} type="Password" placeholder="Enter Password"></TextInput>
       </View>
     </View>
-  )
+  );
 }
 
-const styles =StyleSheet.create({
-input:{
-  padding:15,
-  borderWidth:1,
-  borderRadius:15,
-  borderColor:Colors.GRAY,
-  fontFamily:'outfit'
-}
-})
+const styles = StyleSheet.create({
+  input: {
+    padding: 15,
+    borderWidth: 1,
+    borderRadius: 15,
+    borderColor: Colors.GRAY,
+    fontFamily: "outfit",
+  },
+});
