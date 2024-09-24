d4ecb439f2eaaa4087d63628137e9469fbd08ec3
martinondiwa <surekondiwa@gmail.com>
2024-09-24 09:34:00 +0300

    out

--- Ai_TripGwiji/app/_layout.tsx
+++ Ai_TripGwiji/app/_layout.tsx
@@ -1,7 +1,12 @@
 import { Stack } from "expo-router";
-
+import {useFonts} from "expo-font"
 export default function RootLayout() {
+useFonts({
+'outfit':require('./../assets/fonts/Outfit-Regular'),
+'outfit-mediunm':require('./../assets/fonts/Outfit-Medium'),
+'outfit-bold':require('./../assets/fonts/Outfit-Bold'),
 
+})
 
   return (
     <Stack>
