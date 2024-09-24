7c084ff50914748b538a36e282bd7c4986b5879d
martinondiwa <surekondiwa@gmail.com>
2024-09-24 23:48:19 +0300

    as

--- Ai_TripGwiji/app/(tabs)/_layout.jsx
+++ Ai_TripGwiji/app/(tabs)/_layout.jsx
@@ -1,13 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import EvilIcons from '@expo/vector-icons/EvilIcons';
 
 export default function TabLayout() {
   return (
 <Tabs screenOptions={{
   headerShown:false
 }}>
-  <Tabs.Screen name="mytrip"/>
+  <Tabs.Screen name="mytrip"
+  options={{
+    <EvilIcons name="location" size={24} color="black" />
+  }}
+  />
   <Tabs.Screen name="discover"/>
   <Tabs.Screen name="profile"/>
 </Tabs>
