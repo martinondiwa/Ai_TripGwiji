4aebd3bc16444e3dbdd3706668e5a89a1cb0e602
martinondiwa <surekondiwa@gmail.com>
2024-09-24 23:33:24 +0300

    dfgh

--- Ai_TripGwiji/app/_layout.tsx
+++ Ai_TripGwiji/app/_layout.tsx
@@ -1,21 +1,18 @@
 import { Text, View } from "react-native";
-import Login from './../Components/Login'
-import {auth} from './../configs/FirebaseConfig'
+import Login from './../Components/Login';
+import { auth } from './../configs/FirebaseConfig';
 import { Redirect, Stack } from "expo-router";
+
 export default function RootLayout() {
-  'outfit-medium':require('./../assets/fonts/Outfit-Medium.ttf'),
-  'outfit-bold':require('./../assets/fonts/Outfit-Bold.ttf')
-})
-  
-  const user=auth.currentUser;
+  // Remove the erroneous object and extra parenthesis
+  const user = auth.currentUser;
 
   return (
- <Stack screenOptions={{
-  headerShown:false
- }}>
-  {/**<Stack.Screen name="index" options={{headerShown:false}}/>*/}
-
-  <Stack.Screen name="(tabs)"/>
- </Stack>
+    <Stack screenOptions={{
+      headerShown: false
+    }}>
+      {/**<Stack.Screen name="index" options={{headerShown:false}}/>*/}
+      <Stack.Screen name="(tabs)" />
+    </Stack>
   );
 }
