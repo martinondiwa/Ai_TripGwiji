d8e0cc40728772184b3242f2e7c8c294290c3768
martinondiwa <surekondiwa@gmail.com>
2024-09-24 20:41:17 +0300

    cmm

--- Ai_TripGwiji/app/index.tsx
+++ Ai_TripGwiji/app/index.tsx
@@ -1,6 +1,8 @@
 import { Text, View } from "react-native";
 import Login from './../Components/Login'
+import {auth} from './../configs/FirebaseConfig'
 export default function Index() {
+  const user=auth.currentUser;
   return (
    <View
    style={{flex:1,
