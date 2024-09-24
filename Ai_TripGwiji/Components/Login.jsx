475242956290e981f06b17d9f91e6ee1cc45aa00
martinondiwa <surekondiwa@gmail.com>
2024-09-24 11:56:01 +0300

    ert

--- Ai_TripGwiji/Components/Login.jsx
+++ Ai_TripGwiji/Components/Login.jsx
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
     borderTopLeftRadius:20,
     borderTopRightRadius:30,
     height:'100%',
-    padding: 15
+    padding: 25
   }, 
   button: {
     padding: 15,
