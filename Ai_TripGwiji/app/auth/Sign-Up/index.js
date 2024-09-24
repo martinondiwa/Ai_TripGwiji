66313b87098287474688341e9665b1bdfbd4185f
martinondiwa <surekondiwa@gmail.com>
2024-09-24 15:23:10 +0300

    asdfgh

--- Ai_TripGwiji/app/auth/Sign-Up/index.js
+++ Ai_TripGwiji/app/auth/Sign-Up/index.js
@@ -1,7 +1,8 @@
-import { View, Text } from 'react-native'
+import { View, Text , TextInput} from 'react-native'
 import React, { useEffect } from 'react'
 import { useNavigation } from 'expo-router';
 import { Colors } from "./../../../constants/Colors"; 
+import { StyleSheet } from 'react-native';
 
 export default function SignUp() {
   const navigation=useNavigation();
@@ -24,6 +25,36 @@ export default function SignUp() {
       fontSize:35
       }}
       >Create new Account</Text>
+
+      {/**Email */}
+      <View
+        style={{
+          marginTop: 50,
+        }}
+      >
+        <Text style={{ fontFamily: "outfit" }}>Email</Text>
+        <TextInput style={styles.input} placeholder="Enter Email"></TextInput>
+      </View>
+            {/**Password */}
+      <View
+        style={{
+          marginTop: 20,
+        }}
+      >
+        <Text style={{ fontFamily: "outfit" }}>Password</Text>
+        <TextInput style={styles.input} secureTextEntry={true} placeholder="Enter Password"></TextInput>
+      </View>
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  input: {
+    padding: 15,
+    borderWidth: 1,
+    borderRadius: 15,
+    borderColor: Colors.GRAY,
+    fontFamily: "outfit",
+  },
+});
