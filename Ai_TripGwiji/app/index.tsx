22b44888306a340c3201cdd65b4be947cae783d2
martinondiwa <surekondiwa@gmail.com>
2024-09-24 08:57:05 +0300

    trip

--- Ai_TripGwiji/app/index.tsx
+++ Ai_TripGwiji/app/index.tsx
@@ -5,11 +5,10 @@ export default function Index() {
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Subscrie to TripGwiji</Text>
     </View>
   );
 }
