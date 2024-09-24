a96322971b4234a916d46c880f16698d9472b0db
martinondiwa <surekondiwa@gmail.com>
2024-09-24 13:46:07 +0300

    sign

--- Ai_TripGwiji/app/auth/sign-in/index.js
+++ Ai_TripGwiji/app/auth/sign-in/index.js
@@ -1,16 +1,27 @@
 import { View, Text } from 'react-native'
 import React, { useEffect } from 'react'
+import {useNavigation} from 'expo-router'
 
 export default function SignIn() {
-  const navigation=useEffect();
+  const navigation=useNavigation();
    useEffect(()=>{
-    navigation.set0ptions({
+    navigation.setOptions({
       headershown:false
     })
    },[])
   return (
-    <View>
-      <Text>Sign In</Text>
+    <View
+     style={{
+      padding:25,
+      margin:30
+     }}
+    >
+      <Text
+      style={{
+        fontFamily:'outfit-bold',
+        fontSize:30,
+      }} 
+      >Let's Sign You in</Text>
     </View>
   )
 }
