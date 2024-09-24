f45284810e17daac285cba7cec95f6879e1f9062
martinondiwa <surekondiwa@gmail.com>
2024-09-24 19:15:05 +0300

    sign

--- Ai_TripGwiji/app/auth/sign-in/index.js
+++ Ai_TripGwiji/app/auth/sign-in/index.js
@@ -15,6 +15,19 @@ export default function SignIn() {
       headershown: false,
     });
   }, []);
+
+  const onSignIn=()=>{
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+  }
   return (
     <View
       style={{
