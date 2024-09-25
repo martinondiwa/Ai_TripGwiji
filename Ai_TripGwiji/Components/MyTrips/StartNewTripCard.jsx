c9c060a24fd663f67a5d31006435556030999b13
martinondiwa <surekondiwa@gmail.com>
2024-09-25 08:58:15 +0300

    location

--- Ai_TripGwiji/Components/MyTrips/StartNewTripCard.jsx
+++ Ai_TripGwiji/Components/MyTrips/StartNewTripCard.jsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Entypo from '@expo/vector-icons/Entypo';
 
 export default function StartNewTripCard() {
   return (
     <View>
       <Text>StartNewTripCard</Text>
+      <Entypo name="location" size={24} color="black" />
     </View>
   )
 }
