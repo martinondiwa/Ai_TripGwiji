6362f6f4633c620220ace320d4ba118d418eebed
martinondiwa <surekondiwa@gmail.com>
2024-09-25 07:55:41 +0300

    color

--- Ai_TripGwiji/app/(tabs)/_layout.jsx
+++ Ai_TripGwiji/app/(tabs)/_layout.jsx
@@ -16,7 +16,7 @@ export default function TabLayout() {
   <Tabs.Screen name="mytrip"
   options={{
     tabBarLabel:'My Trip',
-    tabBarIcon:({color})=><EvilIcons name="location" size={24} color="black" />
+    tabBarIcon:({color})=><EvilIcons name="location" size={24} color={color} />
   }}
   />
   <Tabs.Screen name="discover"/>
