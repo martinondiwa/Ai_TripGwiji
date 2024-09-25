4dbfe7180a88de5f0af16659e3b55f3f12dd66fd
martinondiwa <surekondiwa@gmail.com>
2024-09-25 23:01:17 +0300

    login

--- Ai_TripGwiji/app/index.tsx
+++ Ai_TripGwiji/app/index.tsx
@@ -1,6 +1,7 @@
 import { Text, View } from "react-native";
 import { auth } from './../configs/FirebaseConfig';
 import { Redirect } from "expo-router";
+import Login from "@/Components/Login";
 
 export default function Index() {
   const user = auth.currentUser;
@@ -12,9 +13,15 @@ export default function Index() {
 
   // If user exists, show their trips or any other content
   return (
-    <View style={{ flex: 1 }}>
-      {/* You can place content here for logged-in users */}
-      <Text>Welcome to your trip page!</Text>
-    </View>
+    
+     <View 
+     style={{ flex:1, }} 
+     > 
+     {user? 
+     <Redirect href={'/mytrip'}/>: 
+     <Login/> 
+     } 
+     </View> 
+    
   );
 }
