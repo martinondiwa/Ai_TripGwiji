44c19f33873cbb5fef272bdd7478a8b9b7163414
martinondiwa <surekondiwa@gmail.com>
2024-09-25 08:49:47 +0300

    usestate

--- Ai_TripGwiji/app/(tabs)/mytrip.jsx
+++ Ai_TripGwiji/app/(tabs)/mytrip.jsx
@@ -1,9 +1,12 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { Colors } from '../../constants/Colors'
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function MyTrip() {
+
+  const [userTrips,setUserTrips]=useState();
+
   return (
     <View 
     style={{
