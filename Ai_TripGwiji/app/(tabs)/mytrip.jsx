9937ed3fd02491082bb6533c4313981bd12cf3a0
martinondiwa <surekondiwa@gmail.com>
2024-09-25 08:55:55 +0300

    newtrip

--- Ai_TripGwiji/app/(tabs)/mytrip.jsx
+++ Ai_TripGwiji/app/(tabs)/mytrip.jsx
@@ -1,37 +1,43 @@
-import { View, Text } from 'react-native'
-import React, { useState } from 'react'
-import { Colors } from '../../constants/Colors'
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { Colors } from "../../constants/Colors";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import StartNewTripCard from "../../Components/MyTrips/StartNewTripCard";
 
 export default function MyTrip() {
-
-  const [userTrips,setUserTrips]=useState([]);
+  const [userTrips, setUserTrips] = useState([]);
 
   return (
-    <View 
-    style={{
-      padding:25,
-      paddingTop:55,
-      backgroundColor:Colors.WHITE,
-      height:'100%'
-    }}
-    >
     <View
-    style={{
-      display:'flex',
-      flexDirection:'row',
-      alignContent:'center',
-      justifyContent:'space-between'
-    }} 
+      style={{
+        padding: 25,
+        paddingTop: 55,
+        backgroundColor: Colors.WHITE,
+        height: "100%",
+      }}
     >
-    <Text
-    style={{
-      fontFamily:'outfit-bold',
-      fontSize:35
-    }}
-    >My Trips</Text>
-    <Ionicons name="add-circle-outline" size={50} color="black" />
-  </View>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignContent: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "outfit-bold",
+            fontSize: 35,
+          }}
+        >
+          My Trips
+        </Text>
+        <Ionicons name="add-circle-outline" size={50} color="black" />
+      </View>
+      {
+        userTrips?.length==0?
+        <StartNewTripCard/>:null
+      }
     </View>
-  )
+  );
 }
