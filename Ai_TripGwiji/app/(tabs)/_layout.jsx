90f0be357e2ab1f40ad3f25d5a5b5248e0d4083e
martinondiwa <surekondiwa@gmail.com>
2024-09-25 08:00:51 +0300

    ccv

--- Ai_TripGwiji/app/(tabs)/_layout.jsx
+++ Ai_TripGwiji/app/(tabs)/_layout.jsx
@@ -2,14 +2,14 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import {Tabs} from 'expo-router'
 import EvilIcons from '@expo/vector-icons/EvilIcons';
-import {colors} from './../../constants/Colors'
+import {Colors} from './../../constants/Colors'
 
 
 export default function TabLayout() {
   return (
 <Tabs screenOptions={{
   headerShown:false,
-  tabBarActiveTintColor:Colors
+  tabBarActiveTintColor:Colors.PRIMARY
 }}>
   <Tabs.Screen name="mytrip"
   options={{
