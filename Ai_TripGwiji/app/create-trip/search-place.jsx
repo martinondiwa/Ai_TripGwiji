f4388694bb40dc013291f3dde781f0a26befb10b
martinondiwa <surekondiwa@gmail.com>
2024-09-26 08:06:42 +0300

    style

--- Ai_TripGwiji/app/create-trip/search-place.jsx
+++ Ai_TripGwiji/app/create-trip/search-place.jsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React, { useEffect } from 'react'
 import { useNavigation } from 'expo-router'
+import { Colors } from '../../constants/Colors';
 
 export default function SearchPlace() {
 
@@ -16,7 +17,13 @@ export default function SearchPlace() {
     },[])
 
   return (
-    <View>
+    <View 
+    style={{
+        padding:25,
+        paddingTop:75,
+        backgroundColor:Colors.WHITE,
+        height:'100%'
+    }}>
       <Text>Search Place</Text> 
     </View>
   )
