ee8f91231871cb363df6fc69236199e812fa30b0
martinondiwa <surekondiwa@gmail.com>
2024-09-26 11:01:22 +0300

    df

--- Ai_TripGwiji/app/create-trip/search-place.jsx
+++ Ai_TripGwiji/app/create-trip/search-place.jsx
@@ -1,69 +1,69 @@
-import { View, Text } from 'react-native'
-import React, { useContext, useEffect } from 'react'
-import { useNavigation, useRouter } from 'expo-router'
-import { Colors } from '../../constants/Colors';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import {CreateTripContext} from './../../context/CreateTripContext'
+import { View, Text } from "react-native";
+import React, { useContext, useEffect } from "react";
+import { useNavigation, useRouter } from "expo-router";
+import { Colors } from "../../constants/Colors";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { CreateTripContext } from "./../../context/CreateTripContext";
 
 export default function SearchPlace() {
+  const navigation = useNavigation();
+  const { tripData, setTripData } = useContext(CreateTripContext);
+  const router = useRouter();
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      headerTransparent: true,
+      headerTitle: "Search Place",
+    });
+  }, []);
 
-    const navigation=useNavigation();
-    const {tripData,setTripData}=useContext(CreateTripContext);
-    const router=useRouter();
-    useEffect(()=>{
-
-        navigation.setOptions({
-            headerShown:true,
-            headerTransparent: true,
-            headerTitle: 'Search Place'
-        })
-    },[])
-
-    useEffect(()=>{
-        console.log(tripData);
-    }),[tripData]
+  useEffect(() => {
+    console.log(tripData);
+  }),
+    [tripData];
 
   return (
-    <View 
-    style={{
-        padding:25,
-        paddingTop:75,
-        backgroundColor:Colors.WHITE,
-        height:'100%'
-    }}>
+    <View
+      style={{
+        padding: 25,
+        paddingTop: 75,
+        backgroundColor: Colors.WHITE,
+        height: "100%",
+      }}
+    >
+      <View
+        styles={{
+          borderWidth: 1,
+          borderRadius: 5,
+          marginTop: 25,
+        }}
+      >
+        <GooglePlacesAutocomplete
+          placeholder="Search"
+          fetchDetails={true}
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data.description);
+            console.log(details?.geometry.location);
+            console.log(details?.photos[0]?.photo_reference);
+            console.log(details?.url);
 
-    
-    <GooglePlacesAutocomplete
-    placeholder='Search'
-    fetchDetails={true}
-    onPress={(data, details = null) => {
-      // 'details' is provided when fetchDetails = true
-      console.log(data.description);
-      console.log(details?.geometry.location);
-      console.log(details?.photos[0]?.photo_reference);
-      console.log(details?.url);
-
-      setTripData({
-        locationInfo:{
-            name:data.description,
-            coordinates:details?.geometry.location,
-            photoRef:details?.photos[0]?.photo_reference,
-            url:details?.url
-        }
-      })
-      router.push('create-trip/selectTraveler')
-    }}
-    query={{
-      key: process.env.XP_GOOGLE_PLACE_API_KEY,
-      language: 'en',
-    }}
-
-    styles={{
-        borderWidth:1,
-        borderRadius:5,
-        marginTop:25
-    }}
-  />
+            setTripData({
+              locationInfo: {
+                name: data.description,
+                coordinates: details?.geometry.location,
+                photoRef: details?.photos[0]?.photo_reference,
+                url: details?.url,
+              },
+            });
+            router.push("create-trip/selectTraveler");
+          }}
+          query={{
+            key: process.env.XP_GOOGLE_PLACE_API_KEY,
+            language: "en",
+          }}
+        />
+      </View>
     </View>
-  )
+  );
 }
