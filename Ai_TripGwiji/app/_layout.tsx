2e08473187e5b19b8a86d71857f9f37a823fa766
martinondiwa <surekondiwa@gmail.com>
2024-09-26 09:12:34 +0300

    sdf

--- Ai_TripGwiji/app/_layout.tsx
+++ Ai_TripGwiji/app/_layout.tsx
@@ -9,6 +9,8 @@ export default function RootLayout() {
  
   useFonts({
     'outfit':require('./../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-medium':require('./../assets/fonts/Outfit-Medium.ttf'),
+    'outfit-bold':require('./../assets/fonts/Outfit-Bold.ttf'),
   })
   const user = auth.currentUser;
 
