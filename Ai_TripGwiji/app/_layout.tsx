354de5bf07f7d23956c8fbf33348ae9d1e24c739
martinondiwa <surekondiwa@gmail.com>
2024-09-26 09:01:40 +0300

    cont

--- Ai_TripGwiji/app/_layout.tsx
+++ Ai_TripGwiji/app/_layout.tsx
@@ -2,12 +2,13 @@ import { Text, View } from "react-native";
 import Login from './../Components/Login';
 import { auth } from './../configs/FirebaseConfig';
 import { Redirect, Stack } from "expo-router";
-
+import {CreateTripContext} from  './../context/CreateTripContext'
 export default function RootLayout() {
   // Remove the erroneous object and extra parenthesis
   const user = auth.currentUser;
 
   return (
+    <CreatTripContext
     <Stack screenOptions={{
       headerShown: false
     }}>
