6f90a6066069fb2c2dec3dc1378127821a4b8025
martinondiwa <surekondiwa@gmail.com>
2024-09-26 07:51:59 +0300

    router

--- Ai_TripGwiji/Components/MyTrips/StartNewTripCard.jsx
+++ Ai_TripGwiji/Components/MyTrips/StartNewTripCard.jsx
@@ -38,7 +38,7 @@ export default function StartNewTripCard() {
       >Looks like it's time to plan a new travel experience! Get Started below</Text>
 
       <TouchableOpacity 
-      onPress={()=>}
+      onPress={()=>router.push}
       style={{
         padding:10,
         backgroundColor:Colors.PRIMARY,
