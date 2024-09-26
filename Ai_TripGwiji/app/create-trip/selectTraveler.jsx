1e77f68e5844dd0a28372575c890ff3f658f5bde
martinondiwa <surekondiwa@gmail.com>
2024-09-26 12:42:30 +0300

    dfg

--- Ai_TripGwiji/app/create-trip/selectTraveler.jsx
+++ Ai_TripGwiji/app/create-trip/selectTraveler.jsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React, { useEffect } from 'react'
 import { useNavigation} from 'expo-router'
+import { Colors } from '../../constants/Colors';
 
 export default function selectTraveler() {
 
@@ -14,7 +15,13 @@ export default function selectTraveler() {
       })
     },[])
   return (
-    <View>
+    <View
+    style={{
+        padding:25,
+        paddingTop:75,
+        backgroundColor:Colors.GRAY
+    }}
+    >
       <Text>selectTraveler</Text>
     </View>
   )
