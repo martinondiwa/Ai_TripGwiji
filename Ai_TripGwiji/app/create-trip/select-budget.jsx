26977af722a63279aaba2afd63244f2beab39cf3
martinondiwa <surekondiwa@gmail.com>
2024-09-28 10:15:11 +0300

    as

--- Ai_TripGwiji/app/create-trip/select-budget.jsx
+++ Ai_TripGwiji/app/create-trip/select-budget.jsx
@@ -1,7 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useNavigation } from 'expo-router'
 
 export default function SelectBudget() {
+    const navigation=useNavigation();
+
+    
   return (
     <View>
       <Text>select-budget</Text>
