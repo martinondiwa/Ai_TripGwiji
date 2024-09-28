47b11af910785d32b9861490666a4ed2056f4d40
martinondiwa <surekondiwa@gmail.com>
2024-09-28 07:22:33 +0300

    sdf

--- Ai_TripGwiji/app/create-trip/select-dates.jsx
+++ Ai_TripGwiji/app/create-trip/select-dates.jsx
@@ -5,6 +5,14 @@ import { useNavigation } from 'expo-router'
 export default function SelectDates() {
 
   const navigation=useNavigation();
+
+  useEffect(()=>{
+    navigation.setOptions({
+      headerShown:true,
+      headerTransparent:true,
+      headerTitle: ''
+    })
+  },[])
   return (
     <View>
       <Text>select dates</Text>
