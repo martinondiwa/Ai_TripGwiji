72931f774947a41ec82be46a87628c3cd90ffc50
martinondiwa <surekondiwa@gmail.com>
2024-09-28 15:09:54 +0300

    dd

--- Ai_TripGwiji/app/create-trip/generate-trip.jsx
+++ Ai_TripGwiji/app/create-trip/generate-trip.jsx
@@ -1,10 +1,24 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Colors } from '../../constants/Colors'
 
 export default function GenerateTrip() {
   return (
-    <View>
-      <Text>generate-trip</Text>
+    <View
+    style={{
+        padding:25,
+        paddingTop:75,
+        backgroundColor:Colors.GRAY,
+        height:'100%'
+    }}
+    >
+      <Text
+      style={{
+        fontFamily:'outfit-bold',
+        fontSize:35,
+        textAlign:'center'
+      }}
+      >generate-trip</Text>
     </View>
   )
 }
