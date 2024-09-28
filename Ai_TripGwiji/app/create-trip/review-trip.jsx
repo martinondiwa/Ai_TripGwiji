ea41e7da2b46d23d5c238eff756b56a940fd195a
martinondiwa <surekondiwa@gmail.com>
2024-09-28 13:30:35 +0300

    travel

--- Ai_TripGwiji/app/create-trip/review-trip.jsx
+++ Ai_TripGwiji/app/create-trip/review-trip.jsx
@@ -1,75 +1,120 @@
-import { View, Text } from 'react-native'
-import React, { useContext, useEffect, useState } from 'react'
-import { useNavigation } from 'expo-router'
-import { Colors } from '../../constants/Colors';
-import EvilIcons from '@expo/vector-icons/EvilIcons';
-import { CreateTripContext } from '../../context/CreateTripContext';
-
+import { View, Text } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigation } from "expo-router";
+import { Colors } from "../../constants/Colors";
+import EvilIcons from "@expo/vector-icons/EvilIcons";
+import { CreateTripContext } from "../../context/CreateTripContext";
 
 export default function ReviewTrip() {
+  const navigation = useNavigation();
+  const { tripData, setTripData } = useContext(CreateTripContext);
 
-    const navigation=useNavigation();
-    const {tripData,setTripData}=useContext(CreateTripContext);
-
-    useEffect(()=>{
-        navigation.setOptions({
-            headerShown:true,
-            headerTransparent:true,
-            headerTitle:''
-        })
-    },[])
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      headerTransparent: true,
+      headerTitle: "",
+    });
+  }, []);
   return (
     <View
-    style={{
-        padding:25,
-        paddingTop:75,
-        backgroundColor:Colors.GRAY,
-        height:'100%'
-    }}
+      style={{
+        padding: 25,
+        paddingTop: 75,
+        backgroundColor: Colors.GRAY,
+        height: "100%",
+      }}
     >
       <Text
-      style={{
-        fontFamily:'outfit-bold',
-        fontSize:35,
-        marginTop:20
-      }}
-      >Review your trip</Text>
-     <View style={{
-        marginTop:20
-     }}>
-     <Text
-     style={{
-        fontFamily:'outfit-bold',
-        fontSize:20
-     }}
-     > Before generating your trip, please riview your selection</Text>
-     </View>
-     <View>
-     <View style={{
-        marginTop:20,
-        display:'flex',
-        flexDirection:'row',
-        gap:20
-     }}>
-     <EvilIcons name="location" size={34} color="black" />
-     
-     <View>
-     <Text
-     style={{
-        fontFamily:'outfit-regular',
-        fontSize:20,
-        color:'goldenbrown'
-     }}
-     >Destination</Text>
-     <Text
-    style={{
-         fontFamily:'outfit-medium',
-         fontSize:20
-    }}
-     >{tripData?.locationInfo?.name}</Text>
-     </View>
-     </View>
-     </View>
+        style={{
+          fontFamily: "outfit-bold",
+          fontSize: 35,
+          marginTop: 20,
+        }}
+      >
+        Review your trip
+      </Text>
+      <View
+        style={{
+          marginTop: 20,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "outfit-bold",
+            fontSize: 20,
+          }}
+        >
+          {" "}
+          Before generating your trip, please riview your selection
+        </Text>
+      </View>
+      <View>
+        <View
+          style={{
+            marginTop: 20,
+            display: "flex",
+            flexDirection: "row",
+            gap: 20,
+          }}
+        >
+          <EvilIcons name="location" size={34} color="black" />
+
+          <View>
+            <Text
+              style={{
+                fontFamily: "outfit-regular",
+                fontSize: 20,
+                color: "goldenbrown",
+              }}
+            >
+              Destination
+            </Text>
+            <Text
+              style={{
+                fontFamily: "outfit-medium",
+                fontSize: 20,
+              }}
+            >
+              {tripData?.locationInfo?.name}
+            </Text>
+          </View>
+        </View>
+          
+    
+        <View
+          style={{
+            marginTop: 20,
+            display: "flex",
+            flexDirection: "row",
+            gap: 20,
+          }}
+        >
+          <EvilIcons name="location" size={34} color="black" />
+
+          <View>
+            <Text
+              style={{
+                fontFamily: "outfit-regular",
+                fontSize: 20,
+                color: "goldenbrown",
+              }}
+            >
+              Travel Date
+            </Text>
+            <Text
+              style={{
+                fontFamily: "outfit-medium",
+                fontSize: 20,
+              }}
+            >
+              {tripData?.locationInfo?.name}
+            </Text>
+          </View>
+        </View>
+
+
+      </View>
     </View>
-  )
+  );
 }
