17ededac6ce27c27c07eef9b7e81eacb81b7ae08
martinondiwa <surekondiwa@gmail.com>
2024-09-28 07:17:19 +0300

    Nav

--- Ai_TripGwiji/app/_layout.jsx
+++ Ai_TripGwiji/app/_layout.jsx
@@ -19,7 +19,7 @@ export default function RootLayout() {
   return (
     <CreateTripContext.Provider value={{tripData,setTripData}}>
     <Stack screenOptions={{
-      headerShown: false
+      headerShown: true
     }}>
       {/**<Stack.Screen name="index" options={{headerShown:false}}/>*/}
       <Stack.Screen name="(tabs)" />
