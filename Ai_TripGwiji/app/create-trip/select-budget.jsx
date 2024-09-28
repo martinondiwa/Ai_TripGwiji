6c0c26cb4e6f893c0ec358d21c86f08a29a71848
martinondiwa <surekondiwa@gmail.com>
2024-09-28 10:17:37 +0300

    gr

--- Ai_TripGwiji/app/create-trip/select-budget.jsx
+++ Ai_TripGwiji/app/create-trip/select-budget.jsx
@@ -1,11 +1,17 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigation } from 'expo-router'
 
 export default function SelectBudget() {
     const navigation=useNavigation();
 
-    
+    useEffect(()=>{
+        navigation.setOptions({
+            headerShown:true,
+            headerTransparent:true,
+            headerTitle:''
+        })
+    },[])
   return (
     <View>
       <Text>select-budget</Text>
