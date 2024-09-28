c074edd7b3965a67ddf2a1dbfafca5531f7159c3
martinondiwa <surekondiwa@gmail.com>
2024-09-28 12:32:08 +0300

    asd

--- Ai_TripGwiji/app/create-trip/review-trip.jsx
+++ Ai_TripGwiji/app/create-trip/review-trip.jsx
@@ -1,7 +1,12 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigation } from 'expo-router'
 
 export default function ReviewTrip() {
+
+    const navigation=useNavigation();
+
+    useEffect(())
   return (
     <View>
       <Text>review-trip</Text>
