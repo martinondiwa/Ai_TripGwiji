a2a793181aaf7178ea43e03fb02359d7d5dbdc4d
martinondiwa <surekondiwa@gmail.com>
2024-09-28 08:18:43 +0300

    asd

--- Ai_TripGwiji/app/create-trip/select-dates.jsx
+++ Ai_TripGwiji/app/create-trip/select-dates.jsx
@@ -1,39 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useNavigation } from 'expo-router'
-import { Colors } from '../../constants/Colors';
-import { useEffect } from 'react';
+import { View, Text } from "react-native";
+import React from "react";
+import { useNavigation } from "expo-router";
+import { Colors } from "../../constants/Colors";
+import { useEffect } from "react";
 import CalendarPicker from "react-native-calendar-picker";
 
 export default function SelectDates() {
+  const navigation = useNavigation();
 
-  const navigation=useNavigation();
-
-  useEffect(()=>{
+  useEffect(() => {
     navigation.setOptions({
-      headerShown:true,
-      headerTransparent:true,
-      headerTitle: ''
-    })
-  },[])
+      headerShown: true,
+      headerTransparent: true,
+      headerTitle: "",
+    });
+  }, []);
   return (
     <View
-    style={{
-      padding:25,
-      paddingTop:75,
-      backgroundColor: 'aliceblue',
-      height:'100%'
-    }}
+      style={{
+        padding: 25,
+        paddingTop: 75,
+        backgroundColor: "aliceblue",
+        height: "100%",
+      }}
     >
       <Text
-      style={{
-        fontFamily:'outfit-bold',
-        fontSize:35,
-        marginTop:20
-      }}
-      >Travel Dates</Text>
-      <CalendarPicker onDateChange={this.onDateChange} />
-
+        style={{
+          fontFamily: "outfit-bold",
+          fontSize: 35,
+          marginTop: 20,
+        }}
+      >
+        Travel Dates
+      </Text>
+      <View>
+        <CalendarPicker onDateChange={this.onDateChange} />
+      </View>
     </View>
-  )
+  );
 }
